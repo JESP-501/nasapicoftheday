9b6475ad38d3fd5b70fcb8011e9509c758f2ba9b
JESP-501 <espinozajuan2718@gmail.com>
2024-05-03 21:01:37 -0700

    Quick fix
    gotta throw in my old files then install react

---
+++ script.js
@@ -0,0 +1,38 @@
+let dateInput = document.getElementById("dateInput");
+let title = document.getElementById("imageTitle");
+let date = document.getElementById("imageDate");
+let explanation = document.getElementById("explanation");
+let video = document.getElementById("video");
+let image = document.getElementById("image");
+
+const nasaData = (json) => {
+  if (json.media_type === "video") {
+    title.innerHTML = "Title: " + json.title;
+    date.innerHTML = "Date:" + json.date;
+    explanation.innerHTML = "Description: " + json.explanation;
+    video.src = json.url;
+    video.style.display = "block";
+    image.style.display = "none";
+  } else if (json.media_type === "image") {
+    title.innerHTML = "Title: " + json.title;
+    date.innerHTML = "Date: " + json.date;
+    explanation.innerHTML = "Description: " + json.explanation;
+    image.src = json.url;
+    image.style.display = "block";
+    video.style.display = "none";
+  } else {
+    explanation.innerHTML =
+      "You're looking too into the future, choose something within our timeline :(";
+  }
+};
+dateInput.onchange = function (e) {
+  var dateStr = e.target.value;
+  fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=17cBeyBY26TKmYBSq6BO6xNjnEduRiqQgyyjaJia&&date=${dateStr}`
+  )
+    .then((response) => response.json())
+    .then((data) => nasaData(data))
+    .catch((error) => {
+      console.debug(error);
+    });
+};
